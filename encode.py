64fe89d93dbe256cf753721a87098a8d1dff26cf
cmorrison16 <cmorrison16@mail.wou.edu>
2018-09-29 15:21:55 -0700

    finished compression code

--- encode.py
+++ encode.py
@@ -1,2 +1,17 @@
-def encode(in):
-	str encoded = ""
+def encode(input):
+	encoded = ""
+	count = 1
+	x = 0
+	print(len(input))
+	while(x < len(input)-1):
+		if input[x] == input[x+1]:
+			count+=1
+		else:
+			encoded += (input[x] + str(count))
+			count = 1	
+		x+=1
+	if (len(input) > 0):
+		encoded += (input[x] + str(count))	
+	print(encoded)
+	print("HELLO") 
+	return(encoded)
